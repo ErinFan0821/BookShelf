37992ff5ac169907800ad27b5d5852142934506f
techops <fanxiao1987821@gmail.com>
2014-05-18 19:36:35 +0800

    change from contructor to setting in database

--- src/main/java/com/erin/book/Database.java
+++ src/main/java/com/erin/book/Database.java
@@ -9,8 +9,37 @@ import java.sql.Statement;
 
 public class Database {
     private Connection connection = null;
+    private String driver;
+    private String url;
+    private String user;
+    private String password;
 
-    public Database(String driver, String url, String user, String password) {
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public boolean insertData(String sql) throws SQLException {
+        if (null == connection) {
+            createConnection();
+        }
+        Statement statement = connection.createStatement();
+        return  statement.execute(sql);
+    }
+
+    private void createConnection() {
         try {
             Class.forName(driver);
             BasicDataSource basicDataSource = new BasicDataSource();
@@ -23,11 +52,6 @@ public class Database {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public boolean insertData(String sql) throws SQLException {
-        Statement statement = connection.createStatement();
-        return  statement.execute(sql);
     }
 
     public ResultSet selectData(String sql) throws SQLException {
